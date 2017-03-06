56c612eb6cbaed45e71084cc231e59c5551e317f
qualogy-mac <clintatmo>
2017-03-06 15:04:59 -0300

    Added pics

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -9,16 +9,17 @@
           <h1 class="page-header">
             About
           </h1>
-          <span>
-            The Pitbull Box started out with the 2 of us wanting to get back in shape after gaining a lot of post-wedding weight. So we took the conventional approach; we went to the gym.
-            After a short while, unfortunately, we got bored (or lazy) and stopped going.
-            Not quite ready to give up, we did the next thing so many before us have done; we decided to work-out at home. We tried our hand at functional fitness, and to our surprise (and delight) it caught on. A few family members and friends started tagging along. Within a few months more and more people hopped on our little bandwagon, wanting to join in on our newfound hobby.
-            Our passion to help others on their journey to become a healthier, fitter & stronger version of themselves, is what fuels us nowadays.
-            We’re aiming to keep you focussed on the race…
-            Keeping your eye on the prize...
-            Always striving for the max…
-            Pushing you to greatness!
-          </span>
+        </div>
+        <div class="col-lg-4">
+          <img src="/static/gallery_3.jpg" class="image-style">
+        </div>
+        <div class="col-lg-8">
+          <p>The Pitbull Box started out with the 2 of us wanting to get back in shape after gaining a lot of post-wedding weight. So we took the conventional approach; we went to the gym. After a short while, unfortunately, we got bored (or lazy) and stopped going. Not quite ready to give up, we did the next thing so many before us have done; we decided to work-out at home. We tried our hand at functional fitness, and to our surprise (and delight) it caught on.</p>
+          <p>A few family members and friends started tagging along. Within a few months more and more people hopped on our little bandwagon, wanting to join in on our newfound hobby.</p>
+          <p>Our passion to help others on their journey to become a healthier, fitter &amp; stronger version of themselves, is what fuels us nowadays. We&rsquo;re aiming to keep you focussed on the race&hellip;</p>
+          <p>Keeping your eye on the prize...</p>
+          <p>Always striving for the max&hellip;</p>
+          <p>Pushing you to greatness!</p>
         </div>
       </div>
       <!-- /.row -->
@@ -40,4 +41,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+  .image-style {
+    display:inline;
+    width: 300px;
+  }
 </style>
